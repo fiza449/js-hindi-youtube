a2e529c258da0c3823be1a2dddac73886160b8a2
fiza449 <fizaansari667789@gmail.com>
2024-12-04 09:01:09 +0000

    ConversionofData

---
+++ 01_Basics/data_conversion.js
@@ -0,0 +1,30 @@
+let score  = "Hitesh";
+let InNumber = Number(score);
+//console.log(typeof score)
+//console.log(InNumber);
+//console.log(typeof (InNumber));
+
+/*
+String to Number Conversion
+ "33"(a string): Can be converted in to number
+ "33abc": NaN (Not a number) but its type is number
+ true: Converted into 1
+ false: Converted into 0
+*/
+
+let LoggedIn = 0;
+//console.log(typeof LoggedIn);
+let InBoolean = Boolean(LoggedIn);
+//console.log(typeof InBoolean);
+//console.log(InBoolean);
+/*
+Number/String Converted into Boolean:
+1 => true, 0 => false
+"" => false , "fiza" => true
+*/
+
+let num1 = 33;
+let StringNumm = String(num1)
+console.log(typeof num1)
+console.log(typeof StringNumm)
+console.log(StringNumm);
