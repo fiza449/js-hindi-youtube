88b74364f503a2ccdb5622a733625508a2a08538
fiza449 <fizaansari667789@gmail.com>
2024-12-04 08:27:06 +0000

    datatpes

---
+++ 01_Basics/datatypes.js
@@ -0,0 +1,17 @@
+"use strict"; //treat all JS code as newer version
+let num1 = 14;
+let num2 = 2.3;
+let str1 = "World";
+let str2 = 'Hello ${str1}';
+let undefined;
+let beAbsent = null;
+console.table([num1,num2,str1,str2,undefined,beAbsent])
+/*
+Datatypes:
+1. Number: both int and float point number
+2. String: sequence of character enclosed in a quote
+3. Boolean: true/false
+4. undefined: A variable that has been declared but not initialize
+5. Null==> respresents the intentional absence of any object value. IT IS AN OBJECT
+6. symbol: represents a unique immutable identifier
+*/
