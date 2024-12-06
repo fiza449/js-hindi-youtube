589d53712ca87fb69760ec4cfb72bb2320ffc336
fiza449 <fizaansari667789@gmail.com>
2024-12-06 08:34:14 +0000

    String Methods

---
+++ Strings/StringsMethod.js
@@ -0,0 +1,15 @@
+//1. at()
+const name = "Fiza-Ansari-Ali";
+console.log(name.at(2));
+//2.big()
+console.log(name.big());
+//3.blink()
+console.log(name.blink());
+//4.bold()
+console.log(name.bold());
+//5.charAt
+console.log(name.charAt(3));
+//6.charCodeAt
+console.log(name.charCodeAt(4));
+//7. padStart()
+console.log(name.padStart)
