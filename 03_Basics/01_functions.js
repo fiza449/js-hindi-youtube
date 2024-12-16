75347a94f4eda0556000100ac2a1f695a1e53d1b
fiza449 <fizaansari667789@gmail.com>
2024-12-16 11:16:47 +0000

    Functions in JS

---
+++ 03_Basics/01_functions.js
@@ -0,0 +1,49 @@
+//Function definition ke time: Parameters 
+//Function calling ke time: Argument
+function sayMyName(){
+    console.log("Fiza")
+}
+sayMyName //--> Bs reference de raha h(bata raha h ke bs function waha rehta h)
+sayMyName(); //--> Calling function: Fiza
+
+//Function with numbers as a parameters
+function addNum(num1,num2){
+    let result = num1+ num2;
+    return result;
+    return num1+num2 //We can just write this
+    console.log("Hi")// after return will not run anything
+}
+console.log(addNum(2,3)) //5
+console.log(addNum()) //NaN (because i didn't passed any arguments)
+console.log(addNum(3,"4")) //34
+console.log(addNum(3,null))// null: 0 (3+0)= 3
+
+
+// function addNumb(num1,num2){
+//  console.log(num1+ num2) //undefined because: we didn't return anything 
+// }
+//  const result = addNumb(2,3)
+//  console.log(result) //undefined
+
+//Function with string parameters and string interpolation
+
+function usermess(username = "sam"){
+    if(username===undefined){ //if will work if this situation is true we can also write (!username)
+        console.log("Please enter the username")
+        return
+    }
+    return `${username} just logged in!`;
+}
+console.log(usermess("Fiza")); //Fiza just logged in
+
+//  IMP: When we don't pass any arguments in string
+console.log(usermess()) //O/p: undefined just logged in!
+//IMP: When we don't pass any arguments in numbers
+console.log(addNum())// O/p: NaN
+
+//" " --> False value
+// undefined --> False
+
+//function usermess(username = "sam") --> Jab koi arguments paas
+// nhi honge to sam just logged in will execute or agr argument 
+// paas ho raha h to it will overwrite sam
