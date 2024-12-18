15ae771ad54c3b79f7d167f84df7bc33b5b313ef
fiza449 <fizaansari667789@gmail.com>
2024-12-18 13:34:57 +0000

    Hoisting

--- 03_Basics/02_scopes.js
+++ 03_Basics/02_scopes.js
@@ -19,5 +19,36 @@ console.log(c) //8 (which is outside the if-scope) is printed NOT 5 (which is in
 /*
 1. Block scope: Variables declared inside a block (enclosed by {}) are only accessible within that block.
   eg:  let and const have block scope
-2. Global scope: Variables defined outside any function or block are in the global scope and can be accessed from anywhere in the code.
+2. Global scope: Variables defined outside any function or block are in the global scope and can be accessed from anywhere in the code.  
 */
+
+//Nested function scopes execution
+function one(){
+  const username = "Fiza"
+  function two(){
+    const website  = "youtube"
+    console.log(username)
+  }
+  // console.log(website);
+  //two() //--> O/P: Fiza
+}
+one() // --> It won't give any o/p
+
+// In nested function: Child function(two()) can access the variable of Parent function(one())but not vice versa
+/*
+In JS, a variable can hold any type of data. JavaScript is a dynamically 
+typed language, meaning variables are not bound to a specific 
+type and can hold any type of value at any point in time
+*/
+console.log(addone(5)); //6 (When we declare function wihtout using variable we can call t before function declaration)
+function addone(num){
+  return num + 1;
+}
+
+console.log(addtwo(4)) //Cannot access 'addtwo' before initialization(as we declare function in variable we cannot call it before declaration)
+const addtwo = function(num){
+  return num + 2;
+}
+
+//HOISTING: It is JavaScript's default behavior of moving declarations 
+// to the top of their scope (script or function) during the compilation phase. 
