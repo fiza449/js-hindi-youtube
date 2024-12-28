3c52cfe499978d22c5331047d3444ac1084e0932
fiza449 <fizaansari667789@gmail.com>
2024-12-28 08:38:47 +0000

    Loops in JS

--- 05_Iterators/01_loops.js
+++ 05_Iterators/01_loops.js
@@ -21,3 +21,55 @@ for (let i = 0; i < myArray.length; i++) {
     const element = myArray[i];
     console.log(element);
 }
+
+//break and continue in JS
+
+for (let index = 0; index <= 20; index++) {
+    if (index ==5) {
+        console.log("5 is detected")
+        break;
+    }
+    console.log(`Value of i: ${index}`);
+}
+// Output: 
+/*
+Value of i: 0
+Value of i: 1
+Value of i: 2
+Value of i: 3
+Value of i: 4
+5 is detected (In break when 5 is detected it EXITS THE WHOLE LOOP) 
+*/
+
+//continue:
+for (let index = 0; index <= 20; index++) {
+    if (index ==5) {
+        console.log("5 is detected")
+        continue;
+    }
+    console.log(`Value of i: ${index}`);
+}
+// Output:
+/*
+Value of i: 0
+Value of i: 1
+Value of i: 2
+Value of i: 3
+Value of i: 4
+5 is detected  In continue JUST 5 IS IGNORED and then the loop continued
+Value of i: 6
+Value of i: 7
+Value of i: 8
+Value of i: 9
+Value of i: 10
+Value of i: 11
+Value of i: 12
+Value of i: 13
+Value of i: 14
+Value of i: 15
+Value of i: 16
+Value of i: 17
+Value of i: 18
+Value of i: 19
+Value of i: 20
+*/
