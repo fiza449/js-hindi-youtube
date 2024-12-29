698f049c3467363e058b4e0e73426b6a1eb1fba0
fiza449 <fizaansari667789@gmail.com>
2024-12-29 07:01:03 +0000

    While loop

---
+++ 05_Iterators/02_whileloops.js
@@ -0,0 +1,13 @@
+// Syntax of while loop
+let index = 0 //Initialization
+while (index<=10) { //condition
+    console.log(`Value of index is ${index}`)
+    index = index  + 2; //increment
+}
+//
+let myArray = ["fiza","shifa","riza"];
+let i = 0
+while (i<myArray.length) {
+  console.log(`Value is ${myArray[i]}`);
+  i = i + 1;
+}
