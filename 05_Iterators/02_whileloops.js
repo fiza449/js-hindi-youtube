56d57bd420e8551814fba6b92925348fd239cedd
fiza449 <fizaansari667789@gmail.com>
2024-12-29 07:31:26 +0000

    While loop

--- 05_Iterators/02_whileloops.js
+++ 05_Iterators/02_whileloops.js
@@ -11,3 +11,10 @@ while (i<myArray.length) {
   console.log(`Value is ${myArray[i]}`);
   i = i + 1;
 }
+
+// do while loop
+let score = 11
+do {
+  console.log(`Score is: ${score}`);
+  score++;
+} while (score<=10); //Score is: 11(condition is false so it will not go in do again.)
